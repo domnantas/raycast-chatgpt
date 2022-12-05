4cb3e7e56c9810ed38426abebe3b6bf4039ca663
Domantas Petrauskas <domantas@zyro.com>
2022-12-05 12:46:45 +0200

    Remake UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,123 +1,151 @@
 import {
-  Form,
   ActionPanel,
   Action,
   getPreferenceValues,
   Icon,
   openCommandPreferences,
-  useNavigation,
-  Detail,
-  // Cache,
+  List,
+  showToast,
+  Toast,
 } from "@raycast/api";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { TextDecoderStream } from "node:stream/web";
 import crypto from "crypto";
 
-interface Preferences {
+type Preferences = {
   chatGPTToken: string;
-}
-
-type Values = {
-  message: string;
 };
 
-type Message = { id: string; value: string };
-
-type ChatGPTResponseProps = {
-  message: Message;
+type Message = {
+  sent: string;
+  received: string;
+  receivedId: string;
 };
-
-// const cache = new Cache();
-
-function ChatGPTResponse(props: ChatGPTResponseProps) {
-  const { chatGPTToken } = getPreferenceValues<Preferences>();
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentMessage, setCurrentMessage] = useState("");
-  // console.log(cache.get("messages"))
-
-  const { message } = props;
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://chat.openai.com/backend-api/conversation", {
-        method: "POST",
-        headers: {
-          accept: "text/event-stream",
-          "accept-language": "en-US,en;q=0.9",
-          authorization: `Bearer ${chatGPTToken}`,
-          "content-type": "application/json",
-        },
-        referrer: "https://chat.openai.com/chat",
-        referrerPolicy: "strict-origin-when-cross-origin",
-        body: JSON.stringify({
-          action: "next",
-          messages: [
-            {
-              id: message.id,
-              role: "user",
-              content: { content_type: "text", parts: [message.value] },
-            },
-          ],
-          parent_message_id: crypto.randomUUID(),
-          model: "text-davinci-002-render",
-        }),
-        mode: "cors",
-        credentials: "include",
-      });
-      const stream = response.body;
-      const textStream = stream?.pipeThrough(new TextDecoderStream());
-      setIsLoading(false);
-      for await (const chunk of textStream) {
-        try {
-          const chunkData = JSON.parse(chunk.replace("data: ", ""));
-          setCurrentMessage(chunkData.message.content.parts[0]);
-        } catch (error) {} // Swallow errors – if JSON.parse is not valid data it should not do anything
-      }
-    })();
-  }, []);
-  return <Detail isLoading={isLoading} markdown={currentMessage} />;
-}
 
 export default function Command() {
-  const { push } = useNavigation();
+  const { chatGPTToken } = getPreferenceValues<Preferences>();
+
   const [messageValue, setMessageValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Record<string, Message>>({});
+  const [conversationId, setConversationId] = useState<string | null>();
+  const [selectedItemId, setSelectedItemId] = useState<string | undefined>();
 
-  const handleSubmit = async (values: Values) => {
-    const message = {
-      value: values.message,
-      id: crypto.randomUUID(),
-    };
+  const sendMessage = async () => {
+    setIsLoading(true);
+    const messageId = crypto.randomUUID();
+    const allMessages = Object.entries(messages);
+    const previousMessageId = allMessages.length
+      ? allMessages[allMessages.length - 1][1].receivedId
+      : crypto.randomUUID();
 
-    // if (cache.has("messages") && cache.get("messages")?.length) {
-    //   const existingMessages = JSON.parse(cache.get("messages")!)
-    //   cache.set("messages", JSON.stringify([
-    //     ...existingMessages,
-    //     message
-    //   ]));
-    // } else {
-    //   cache.set("messages", JSON.stringify([message]))
-    // }
+    const response = await fetch("https://chat.openai.com/backend-api/conversation", {
+      method: "POST",
+      headers: {
+        accept: "text/event-stream",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${chatGPTToken}`,
+        "content-type": "application/json",
+      },
+      referrer: "https://chat.openai.com/chat",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: JSON.stringify({
+        action: "next",
+        messages: [
+          {
+            id: messageId,
+            role: "user",
+            content: { content_type: "text", parts: [messageValue] },
+          },
+        ],
+        ...(conversationId ? { conversation_id: conversationId } : {}),
+        parent_message_id: previousMessageId,
+        model: "text-davinci-002-render",
+      }),
+      mode: "cors",
+      credentials: "include",
+    });
+    const stream = response.body;
+    const textStream = stream?.pipeThrough(new TextDecoderStream());
+    setIsLoading(false);
+    setSelectedItemId(messageId);
 
-    push(<ChatGPTResponse message={message} />);
+    // @ts-ignore
+    for await (const chunk of textStream) {
+      try {
+        const chunkData = JSON.parse(chunk.replace("data: ", ""));
+        if (chunkData?.detail?.code === "token_expired") {
+          showToast({
+            style: Toast.Style.Failure,
+            title: "Token expired",
+            message: "Please update the token in the extension preferences",
+          });
+        } else if (chunkData?.detail) {
+          showToast({
+            style: Toast.Style.Failure,
+            title: "Error",
+            message: chunkData.detail,
+          });
+        }
+
+        if (!conversationId) {
+          setConversationId(chunkData.conversation_id);
+        }
+
+        setMessages({
+          ...messages,
+          [messageId]: {
+            sent: messageValue,
+            received: chunkData.message.content.parts[0],
+            receivedId: chunkData.message.id,
+          },
+        });
+      } catch (error) {}
+    }
+
     setMessageValue("");
   };
 
+  const resetConversation = () => {
+    setConversationId(null);
+    setMessages({});
+    setSelectedItemId(undefined);
+  };
+
   return (
-    <Form
+    <List
+      searchBarPlaceholder="Enter message..."
+      searchText={messageValue}
+      onSearchTextChange={setMessageValue}
+      isLoading={isLoading}
+      selectedItemId={selectedItemId}
       actions={
         <ActionPanel>
-          <Action.SubmitForm title="Send" icon={Icon.Message} onSubmit={handleSubmit} />
-          {/* <Action title="Restart conversation" icon={Icon.Repeat} onAction={() => cache.remove("messages")} /> */}
+          <Action title="Send" icon={Icon.Message} onAction={sendMessage} />
           <Action title="Change ChatGPT token" icon={Icon.Gear} onAction={() => openCommandPreferences()} />
         </ActionPanel>
       }
+      isShowingDetail={true}
     >
-      <Form.TextArea
-        id="message"
-        value={messageValue}
-        onChange={(value) => setMessageValue(value)}
-        placeholder="Enter message..."
-      />
-    </Form>
+      {Object.entries(messages).length ? (
+        Object.entries(messages).map(([messageId, message]) => (
+          <List.Item
+            key={messageId}
+            id={messageId}
+            title={message.sent}
+            detail={<List.Item.Detail markdown={`**You:**\n${message.sent}\n\n**ChatGPT:**\n${message.received}`} />}
+            actions={
+              <ActionPanel>
+                <Action title="Send" icon={Icon.Message} onAction={sendMessage} />
+                <Action title="Reset conversation" icon={Icon.Repeat} onAction={resetConversation} />
+                <Action title="Change ChatGPT token" icon={Icon.Gear} onAction={() => openCommandPreferences()} />
+              </ActionPanel>
+            }
+          />
+        ))
+      ) : (
+        <List.EmptyView icon={Icon.Message} title="No messages" />
+      )}
+    </List>
   );
 }
